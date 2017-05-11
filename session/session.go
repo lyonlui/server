c5e76dd27bf2950be1b9764867fd5941c748b302
lyonlui <lyonlui@gmail.com>
2017-05-11 18:41:51 +0800

    Update session.go

--- session/session.go
+++ session/session.go
@@ -30,25 +30,21 @@ package session
 import (
 	"crypto/rand"
 	"encoding/hex"
-	"errors"
 	"fmt"
 	"io"
 	"log"
-	"net/http"
-	"net/textproto"
-	"net/url"
 	"os"
 	"time"
 )
 
 // Store contains all data for one session process with specific id.
 type Store interface {
-	Set(key, value interface{}) error     //set session value
-	Get(key interface{}) interface{}      //get session value
-	Delete(key interface{}) error         //delete session value
-	SessionID() string                    //back current sessionID
-	SessionRelease(w http.ResponseWriter) // release the resource & save data to provider & return the data
-	Flush() error                         //delete all data
+	Set(key, value interface{}) error //set session value
+	Get(key interface{}) interface{}  //get session value
+	Delete(key interface{}) error     //delete session value
+	SessionID() string                //back current sessionID
+	SessionRelease()                  // release the resource & save data to provider & return the data
+	Flush() error                     //delete all data
 }
 
 // Provider contains global session methods and saved SessionStores.
@@ -87,15 +83,10 @@ type ManagerConfig struct {
 	EnableSetCookie bool   `json:"enableSetCookie,omitempty"`
 	Gclifetime      int64  `json:"gclifetime"`
 	Maxlifetime     int64  `json:"maxLifetime"`
-	//DisableHTTPOnly         bool   `json:"disableHTTPOnly"`
-	//Secure                  bool   `json:"secure"`
 	CookieLifeTime  int    `json:"cookieLifeTime"`
 	ProviderConfig  string `json:"providerConfig"`
 	Domain          string `json:"domain"`
 	SessionIDLength int64  `json:"sessionIDLength"`
-	//EnableSidInHTTPHeader   bool   `json:"EnableSidInHTTPHeader"`
-	//SessionNameInHTTPHeader string `json:"SessionNameInHTTPHeader"`
-	//EnableSidInURLQuery     bool   `json:"EnableSidInURLQuery"`
 }
 
 // Manager contains Provider and its configuration.
@@ -126,44 +117,6 @@ func NewManager(provideName string, cf *ManagerConfig) (*Manager, error) {
 		cf.Maxlifetime = cf.Gclifetime
 	}
 
-	if cf.EnableSidInHTTPHeader {
-		if cf.SessionNameInHTTPHeader == "" {
-			panic(errors.New("SessionNameInHTTPHeader is empty"))
-		}
-
-		strMimeHeader := textproto.CanonicalMIMEHeaderKey(cf.SessionNameInHTTPHeader)
-		if cf.SessionNameInHTTPHeader != strMimeHeader {
-			strErrMsg := "SessionNameInHTTPHeader (" + cf.SessionNameInHTTPHeader + ") has the wrong format, it should be like this : " + strMimeHeader
-			panic(errors.New(strErrMsg))
-		}
-	}
-
-	err := provider.SessionInit(cf.Maxlifetime, cf.ProviderConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	if cf.SessionIDLength == 0 {
-		cf.SessionIDLength = 16
-	}
-
-	return &Manager{
-		provider,
-		cf,
-	}, nil
-}
-
-/*
-func NewManager(provideName string, cf *ManagerConfig) (*Manager, error) {
-	provider, ok := provides[provideName]
-	if !ok {
-		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", provideName)
-	}
-
-	if cf.Maxlifetime == 0 {
-		cf.Maxlifetime = cf.Gclifetime
-	}
-
 	err := provider.SessionInit(cf.Maxlifetime, cf.ProviderConfig)
 	if err != nil {
 		return nil, err
@@ -178,102 +131,20 @@ func NewManager(provideName string, cf *ManagerConfig) (*Manager, error) {
 		cf,
 	}, nil
 
-}*/
-
-// getSid retrieves session identifier from HTTP Request.
-// First try to retrieve id by reading from cookie, session cookie name is configurable,
-// if not exist, then retrieve id from querying parameters.
-//
-// error is not nil when there is anything wrong.
-// sid is empty when need to generate a new session id
-// otherwise return an valid session id.
-func (manager *Manager) getSid(r *http.Request) (string, error) {
-	cookie, errs := r.Cookie(manager.config.CookieName)
-	if errs != nil || cookie.Value == "" {
-		var sid string
-		if manager.config.EnableSidInURLQuery {
-			errs := r.ParseForm()
-			if errs != nil {
-				return "", errs
-			}
-
-			sid = r.FormValue(manager.config.CookieName)
-		}
-
-		// if not found in Cookie / param, then read it from request headers
-		if manager.config.EnableSidInHTTPHeader && sid == "" {
-			sids, isFound := r.Header[manager.config.SessionNameInHTTPHeader]
-			if isFound && len(sids) != 0 {
-				return sids[0], nil
-			}
-		}
-
-		return sid, nil
-	}
-
-	// HTTP Request contains cookie for sessionid info.
-	return url.QueryUnescape(cookie.Value)
 }
 
-/*
-func (manager *Manager) getSid(r *http.Request) (string, error) {
+func Display() {
 
 }
-*/
-// SessionStart generate or read the session id from http request.
-// if session id exists, return SessionStore with this id.
-func (manager *Manager) SessionStart(w http.ResponseWriter, r *http.Request) (session Store, err error) {
-	sid, errs := manager.getSid(r)
-	if errs != nil {
-		return nil, errs
-	}
 
-	if sid != "" && manager.provider.SessionExist(sid) {
-		return manager.provider.SessionRead(sid)
-	}
+// CreatNewSession generate a new session id
+func (manager *Manager) CreatNewSession() (session Store, err error) {
 
 	// Generate a new session
-	sid, errs = manager.sessionID()
-	if errs != nil {
-		return nil, errs
-	}
+	sid, err := manager.sessionID()
 
-	session, err = manager.provider.SessionRead(sid)
 	if err != nil {
 		return nil, err
-	}
-	cookie := &http.Cookie{
-		Name:     manager.config.CookieName,
-		Value:    url.QueryEscape(sid),
-		Path:     "/",
-		HttpOnly: !manager.config.DisableHTTPOnly,
-		Secure:   manager.isSecure(r),
-		Domain:   manager.config.Domain,
-	}
-	if manager.config.CookieLifeTime > 0 {
-		cookie.MaxAge = manager.config.CookieLifeTime
-		cookie.Expires = time.Now().Add(time.Duration(manager.config.CookieLifeTime) * time.Second)
-	}
-	if manager.config.EnableSetCookie {
-		http.SetCookie(w, cookie)
-	}
-	r.AddCookie(cookie)
-
-	if manager.config.EnableSidInHTTPHeader {
-		r.Header.Set(manager.config.SessionNameInHTTPHeader, sid)
-		w.Header().Set(manager.config.SessionNameInHTTPHeader, sid)
-	}
-
-	return
-}
-
-/*
-func (manager *Manager)CreatNewSession()(session Store, err error){
-
-	// Generate a new session
-	sid, errs = manager.sessionID()
-	if errs != nil {
-		return nil, errs
 	}
 
 	session, err = manager.provider.SessionRead(sid)
@@ -281,39 +152,14 @@ func (manager *Manager)CreatNewSession()(session Store, err error){
 		return nil, err
 	}
 
-	if manager.config.CookieLifeTime > 0 {
-		cookie.MaxAge = manager.config.CookieLifeTime
-		cookie.Expires = time.Now().Add(time.Duration(manager.config.CookieLifeTime) * time.Second)
-	}
-
-
-	return
+	return session, nil
 }
-*/
+
 // SessionDestroy Destroy session by its id in http request cookie.
-func (manager *Manager) SessionDestroy(w http.ResponseWriter, r *http.Request) {
-	if manager.config.EnableSidInHTTPHeader {
-		r.Header.Del(manager.config.SessionNameInHTTPHeader)
-		w.Header().Del(manager.config.SessionNameInHTTPHeader)
-	}
+func (manager *Manager) SessionDestroy(sid string) {
 
-	cookie, err := r.Cookie(manager.config.CookieName)
-	if err != nil || cookie.Value == "" {
-		return
-	}
-
-	sid, _ := url.QueryUnescape(cookie.Value)
 	manager.provider.SessionDestroy(sid)
-	if manager.config.EnableSetCookie {
-		expiration := time.Now()
-		cookie = &http.Cookie{Name: manager.config.CookieName,
-			Path:     "/",
-			HttpOnly: !manager.config.DisableHTTPOnly,
-			Expires:  expiration,
-			MaxAge:   -1}
 
-		http.SetCookie(w, cookie)
-	}
 }
 
 // GetSessionStore Get SessionStore by its id.
@@ -325,49 +171,25 @@ func (manager *Manager) GetSessionStore(sid string) (sessions Store, err error) 
 // GC Start session gc process.
 // it can do gc in times after gc lifetime.
 func (manager *Manager) GC() {
+	fmt.Println("GC")
 	manager.provider.SessionGC()
 	time.AfterFunc(time.Duration(manager.config.Gclifetime)*time.Second, func() { manager.GC() })
 }
 
 // SessionRegenerateID Regenerate a session id for this SessionStore who's id is saving in http request.
-func (manager *Manager) SessionRegenerateID(w http.ResponseWriter, r *http.Request) (session Store) {
+func (manager *Manager) SessionRegenerateID(oldsid string) (session Store) {
 	sid, err := manager.sessionID()
 	if err != nil {
 		return
 	}
-	cookie, err := r.Cookie(manager.config.CookieName)
-	if err != nil || cookie.Value == "" {
-		//delete old cookie
-		session, _ = manager.provider.SessionRead(sid)
-		cookie = &http.Cookie{Name: manager.config.CookieName,
-			Value:    url.QueryEscape(sid),
-			Path:     "/",
-			HttpOnly: !manager.config.DisableHTTPOnly,
-			Secure:   manager.isSecure(r),
-			Domain:   manager.config.Domain,
-		}
-	} else {
-		oldsid, _ := url.QueryUnescape(cookie.Value)
-		session, _ = manager.provider.SessionRegenerate(oldsid, sid)
-		cookie.Value = url.QueryEscape(sid)
-		cookie.HttpOnly = true
-		cookie.Path = "/"
-	}
-	if manager.config.CookieLifeTime > 0 {
-		cookie.MaxAge = manager.config.CookieLifeTime
-		cookie.Expires = time.Now().Add(time.Duration(manager.config.CookieLifeTime) * time.Second)
-	}
-	if manager.config.EnableSetCookie {
-		http.SetCookie(w, cookie)
-	}
-	r.AddCookie(cookie)
 
-	if manager.config.EnableSidInHTTPHeader {
-		r.Header.Set(manager.config.SessionNameInHTTPHeader, sid)
-		w.Header().Set(manager.config.SessionNameInHTTPHeader, sid)
+	session, err = manager.provider.SessionRegenerate(oldsid, sid)
+
+	if err != nil {
+		return
 	}
 
-	return
+	return session
 }
 
 // GetActiveSession Get all active sessions count number.
@@ -375,12 +197,6 @@ func (manager *Manager) GetActiveSession() int {
 	return manager.provider.SessionAll()
 }
 
-/*
-// SetSecure Set cookie with https.
-func (manager *Manager) SetSecure(secure bool) {
-	manager.config.Secure = secure
-}
-*/
 func (manager *Manager) sessionID() (string, error) {
 	b := make([]byte, manager.config.SessionIDLength)
 	n, err := rand.Read(b)
@@ -389,21 +205,6 @@ func (manager *Manager) sessionID() (string, error) {
 	}
 	return hex.EncodeToString(b), nil
 }
-
-/*
-// Set cookie with https.
-func (manager *Manager) isSecure(req *http.Request) bool {
-	if !manager.config.Secure {
-		return false
-	}
-	if req.URL.Scheme != "" {
-		return req.URL.Scheme == "https"
-	}
-	if req.TLS == nil {
-		return false
-	}
-	return true
-}*/
 
 // Log implement the log.Logger
 type Log struct {
