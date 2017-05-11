b63b90bed12e3ab9c2687ada91b91acaa488d8a9
lyonlui <lyonlui@gmail.com>
2017-05-11 18:48:16 +0800

    Create session_test.go

---
+++ session/session_test.go
@@ -0,0 +1,40 @@
+package session
+/*
+import (
+  "fmt"
+)
+
+var GlobalSessions *session.Manager
+
+func init() {
+	var config session.ManagerConfig
+	config.CookieName = "GameID"
+	config.Gclifetime = 50
+	config.ProviderConfig = "root:HelloWorld!@/qpaccountsdb"
+	config.SessionIDLength = 16
+
+	GlobalSessions, _ = session.NewManager("mysql", &config)
+	go GlobalSessions.GC()
+}
+
+
+func main() {
+
+	session, err := test.GlobalSessions.CreatNewSession()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+
+	}
+
+	sid := session.SessionID()
+
+	fmt.Println(sid)
+
+	sum := 0
+	for sum < 1 {
+
+	}
+}
+*/
